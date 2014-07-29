390fc33284ee8343e03ae7517c4ac88aa43d4ea4
Brian Warner <bwarner@tffosx.ifn.private>
2014-07-29 16:32:53 -0700

    made it more mem efficient. each url is loaded in browser real time, instead of waitign for crawl to finish.

--- scraper.rb
+++ scraper.rb
@@ -10,39 +10,44 @@ require 'watir-webdriver'
 # @profile.secure_ssl = true
 @browser = Watir::Browser.new :firefox
 
-base_url = "http://thekingdomofgodisin.me"
+base_url = "http://mynuvotv.com"
 puts 'Crawling site'
 Anemone.crawl(base_url) do |a|
   URLS = []
   a.on_every_page do |p|
     if p.html?
       URLS << p.url.to_s
-
-      p URLS
+      URLS.each do |u|
+        @browser.goto(u)
+        if @browser.text.include? "404"
+            p u + " FAILED: page is broken"
+         end
+      end
+      # p URLS
     end
   end
 end
-randomNum = rand(1000..6000)
-filenameOut = "web_scraper/scraper_fails_#{randomNum}_.txt"
-File.new("#{filenameOut}", "w")
-URLS.each do |u|
-  @browser.goto(u)
-  if @browser.text.include? "404"
-    p u + " FAILED: page is broken"
-    begin
-      file = File.open("#{filenameOut}", "a")
-      file.write("#{u} FAILED and is a broken page... \n")
-    rescue IOError => e
-      p "FAILED TO WRITE"
-    ensure
-      file.close unless file == nil
-    end
-  else
-
-  end
-  #More Assertions on template:
-  @browser.image(:src, "styles/images/eo_logo.gif").exists?     #each page has the logo
-  @browser.image(:src, "http://s7.addthis.com/static/btn/v2/lg-share-en.gif").exists?    #each page has the social share icon
-  @browser.div(:id => "link_to_title").exists?      #each page has the div field for linking to the page
-end
-@browser.close
+# randomNum = rand(1000..6000)
+# filenameOut = "web_scraper/scraper_fails_#{randomNum}_.txt"
+# File.new("#{filenameOut}", "w")
+# URLS.each do |u|
+#   @browser.goto(u)
+#   if @browser.text.include? "404"
+#     p u + " FAILED: page is broken"
+#     begin
+#       file = File.open("#{filenameOut}", "a")
+#       file.write("#{u} FAILED and is a broken page... \n")
+#     rescue IOError => e
+#       p "FAILED TO WRITE"
+#     ensure
+#       file.close unless file == nil
+#     end
+#   else
+#
+#   end
+#   #More Assertions on template:
+#   @browser.image(:src, "styles/images/eo_logo.gif").exists?     #each page has the logo
+#   @browser.image(:src, "http://s7.addthis.com/static/btn/v2/lg-share-en.gif").exists?    #each page has the social share icon
+#   @browser.div(:id => "link_to_title").exists?      #each page has the div field for linking to the page
+# end
+# @browser.close
